37c3822139c7353c14c30c674e848ca6f9ad5b8d
Jon Haddad <jon@jonhaddad.com>
2016-02-23 16:40:18 -0800

    updating cli to be similar to pip

--- cdm/cli.py
+++ cdm/cli.py
@@ -1,8 +1,8 @@
 """cdm - Cassandra Data Manager
 
 Usage:
-    cdm list
-    cdm versions <dataset>
+    cdm search [<term>]
+    cdm show <dataset>
     cdm install [--host=<host>] [--keyspace=<keyspace>] [--dry-run] <dataset>
     cdm update
 
@@ -12,24 +12,36 @@ Examples:
 
 import sys
 sys.path.append("")
+import os
+import os.path
 from docopt import docopt
 
-DEBUG=False
+DEBUG = False
+DATASETS_URL = "https://github.com/cassandra-data-manager/cdm/blob/master/datasets.yaml"
+
+CDM_CACHE = os.getenv("CDM_CACHE", os.path.expanduser("~/.cdm"))
 
 def main():
     arguments = docopt(__doc__)
     print arguments
 
-    if arguments["list"]: return list_datasets()
+    try:
+        os.mkdir(CDM_CACHE)
+    except OSError:
+        pass
+
+
+    if arguments["search"]: return list_datasets(arguments["<term>"])
     if arguments["update"]: return update_datasets()
 
     print "Done"
 
-def list_datasets():
+def list_datasets(search):
     print "Available datasets:"
 
 def update_datasets():
     print "Updating datasets"
+
 
 if __name__ == "__main__":
     main()
