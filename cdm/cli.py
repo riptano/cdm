b459dc352f88e2a0061ddd2fc247b31698c93028
Jon Haddad <jon@jonhaddad.com>
2016-02-23 16:25:36 -0800

    working on simple CLI

--- cdm/cli.py
+++ cdm/cli.py
@@ -1,10 +1,39 @@
+"""cdm - Cassandra Data Manager
+
+Usage:
+    cdm list
+    cdm versions <dataset>
+    cdm install [--host=<host>] [--keyspace=<keyspace>] <dataset>
+    cdm update
+
+Options:
+    --debug     Print debugging information
+    --dry-run   Dry run, don't really install anything
+
+Examples:
+    cdm install medialens
+"""
+
 import sys
 sys.path.append("")
+from docopt import docopt
+
+DEBUG=False
 
 def main():
-    print "Generating stuff"
+    arguments = docopt(__doc__)
+    print arguments
+
+    if arguments["list"]: return list_datasets()
+    if arguments["update"]: return update_datasets()
+
     print "Done"
 
+def list_datasets():
+    print "Available datasets:"
+
+def update_datasets():
+    print "Updating datasets"
 
 if __name__ == "__main__":
     main()
