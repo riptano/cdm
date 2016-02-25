b5b7a24921645969441c284fb1e741b7adb64a14
Jon Haddad <jon@jonhaddad.com>
2016-02-24 22:59:03 -0800

    working on file downloads and caching

--- cdm/context.py
+++ cdm/context.py
@@ -1,3 +1,4 @@
+import urllib2
 
 class Context(object):
     session = None
@@ -7,8 +8,22 @@ class Context(object):
     def __init__(self, dataset, session, cache_dir):
         self.dataset = dataset
         self.session = session
+        # add trailing slash
+        if not cache_dir.endswith("/"):
+            cache_dir = cache_dir + "/"
+
         self.cache_dir = cache_dir
 
+
+    def download(self, url, cache=True):
+        """
+        returns a file pointer
+
+        :param url:
+        :param cache:
+        :return:
+        """
+        cache = self.cache_dir + ""
 
 
 
