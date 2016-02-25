012d2d03dd16cacd828418ecbdbdfca8e14821a3
Jon Haddad <jon@jonhaddad.com>
2016-02-24 21:25:15 -0800

    tests

---
+++ tests/test_downloads.py
@@ -0,0 +1,21 @@
+from pytest import fixture
+
+from cdm.context import Context
+from cassandra.cluster import Cluster
+
+
+@fixture(scope="module")
+def context():
+    cluster = Cluster()
+    session = cluster.connect()
+    # create the keyspace
+    ks = 'test_cdm'
+    if 'test_cdm' not in cluster.metadata.keyspaces:
+        q = "create KEYSPACE {} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1}}".format(ks)
+        session.execute(q)
+    session.
+
+
+
+
+
