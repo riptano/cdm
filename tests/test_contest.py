faa6a379771f74de7f3bb7350106b5b096c00acb
Jon Haddad <jon@jonhaddad.com>
2016-02-24 21:49:10 -0800

    preparing session

--- tests/test_contest.py
+++ tests/test_contest.py
@@ -3,17 +3,25 @@ from pytest import fixture
 from cdm.context import Context
 from cassandra.cluster import Cluster
 
+session = None
 
-@fixture(scope="module")
+@fixture
 def context():
-    cluster = Cluster()
-    session = cluster.connect()
-    # create the keyspace
-    ks = 'test_cdm'
-    if 'test_cdm' not in cluster.metadata.keyspaces:
-        q = "create KEYSPACE {} WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1}}".format(ks)
+    global session
+    if not session:
+        cluster = Cluster()
+        session = cluster.connect()
+
+        # create the keyspace
+        ks = 'test_cdm'
+        if 'test_cdm' not in cluster.metadata.keyspaces:
+            q = "create KEYSPACE {} WITH \
+                    replication = {{'class': \
+                    'SimpleStrategy', \
+                    'replication_factor': 1}}".format(ks)
         session.execute(q)
-    session.set_keyspace(ks)
+
+        session.set_keyspace(ks)
 
     return Context(session=session)
 
