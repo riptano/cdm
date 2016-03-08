e1e1d8648ae2139e4324b897829d1c25b00cf801
Jon Haddad <jon@jonhaddad.com>
2016-03-08 12:31:43 -0800

    added context for testing

--- cdm/testing.py
+++ cdm/testing.py
@@ -1,10 +1,30 @@
 import pytest
+import os
+from os.path import join
+from cdm import get_session
+from cdm.context import Context
 
-@pytest.fixture
+
+@pytest.fixture(scope="session")
 def context():
     """
     returns a context with a whole bunch of stuff set up
     :return:
     """
+
     def fin():
         print "Teardown context"
+
+    cache_dir = join(os.getcwd(), ".cdmcache")
+    try:
+        os.mkdir(cache_dir)
+    except:
+        # TODO don't be so silly
+        pass
+
+    c = Context(root=os.getcwd(),
+                dataset="test",
+                session=get_session("test", True),
+                cache_dir=cache_dir)
+
+    return c
