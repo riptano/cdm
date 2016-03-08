b47b0a8f19ac6c099766f35dbfe0b5f772cb31ee
Jon Haddad <jon@jonhaddad.com>
2016-03-08 10:56:25 -0800

    removed install_local

--- cdm/__init__.py
+++ cdm/__init__.py
@@ -4,21 +4,3 @@ import os
 import os.path
 
 
-def install_local(dataset, main):
-    """
-    :param dataset:
-    :param main:
-    :return:
-    """
-
-    if not os.path.exists("cache"):
-        os.mkdir("cache")
-
-    # apply schema change
-
-    c = Context(dataset=dataset,
-                session=get_session(dataset),
-                cache_dir="cache")
-
-    # c.clean_cache()
-    main(c)
