ec77ac8feab7c4f703abd355e04fc3d2f010eea5
Jon Haddad <jon@jonhaddad.com>
2016-04-05 13:56:26 -0700

    progress bar is cool

--- cdm/importer.py
+++ cdm/importer.py
@@ -1,8 +1,12 @@
 class Importer(object):
     session = None
+    dataframe = None
+    fields = None
 
-    def __init__(self, session):
+    def __init__(self, session, dataframe, fields):
         self.session = session
+        self.dataframe = dataframe
+        self.fields = fields
 
     def map(self, f):
         pass
--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,1 +1,14 @@
 from cdm.importer import Importer
+from pandas import DataFrame
+from pytest import fixture
+
+@fixture
+def df():
+    data = [{'name': "Jon", 'hats': 1, 'nickname':"Beast"},
+            {'name': "Luke", 'hats': 50, 'nickname':"Loopsie"}]
+
+    return DataFrame(data)
+
+
+def test_simple_importer(df):
+    pass
