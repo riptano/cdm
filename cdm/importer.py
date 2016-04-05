61983664b6ecad5b51b0eed38d74516880543385
Jon Haddad <jon@jonhaddad.com>
2016-04-04 22:13:19 -0700

    importer

---
+++ cdm/importer.py
@@ -0,0 +1,8 @@
+class Importer(object):
+    session = None
+
+    def __init__(self, session):
+        self.session = session
+
+    def map(self, f):
+        pass
---
+++ tests/test_importer.py
@@ -0,0 +1,1 @@
+from cdm.importer import Importer
