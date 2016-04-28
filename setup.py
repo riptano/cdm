6c77e6600825a78c1d66dc611d2a7303ccb5394d
Jon Haddad <jon@jonhaddad.com>
2016-04-28 12:01:32 -0500

    fixed name for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-        name='Cassandra Dataset Manager',
+        name='cassandra-dataset-manager',
         version='0.1',
         packages=find_packages(),
         url='https://github.com/riptano/cdm',
