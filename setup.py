67d16abb84ed0c14ff9d508e9f456938c347ddc8
Jon Haddad <jon@jonhaddad.com>
2016-04-28 12:58:32 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
         name='cassandra-dataset-manager',
-        version='0.1',
+        version='0.2',
         packages=find_packages(),
         url='https://github.com/riptano/cdm',
         license='License :: OSI Approved :: Apache License',
